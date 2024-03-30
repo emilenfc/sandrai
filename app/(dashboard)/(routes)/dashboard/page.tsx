5cdcb9eeb408c00d43da02d1253c9095f51c9d5f
IYADUKUNZE <iyaemile4@gmail.com>
2024-03-30 16:24:19 +0200

    fit: add card on dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,16 @@
-"use client"
+'use client';
 
-import { Card } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { MessageSquare } from "lucide-react";
+import { Card } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import { useRouter } from 'next/navigation';
 
+import {
+  ArrowRight,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon
+} from 'lucide-react';
 
 const tools = [
   {
@@ -12,9 +19,39 @@ const tools = [
     color: 'text-violet-500',
     bgColor: 'bg-violet-500/10',
     href: '/conversation'
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    color: 'text-emerald-500',
+    bgColor: 'bg-emerald-500/10',
+    href: '/music'
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: 'text-pink-700',
+    bgColor: 'bg-pink-700/10',
+    href: '/image'
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: 'text-orange-700',
+    bgColor: 'bg-orange-700/10',
+    href: '/video'
+  },
+  {
+    label: 'Code Generation',
+    icon: Music,
+    color: 'text-green-700',
+    bgColor: 'bg-green-700/10',
+    href: '/code'
   }
-]
+];
 const DashboardPage = () => {
+  const router = useRouter();
+
   return (
     <div>
       <div className="mb-8 space-y-4">
@@ -22,20 +59,34 @@ const DashboardPage = () => {
           Explore the power of AI
         </h1>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Chat with the smartes AI - Experience the power of AI
+          Chat with the smartes AI - Experience the power of
+          AI
         </p>
       </div>
-      <div className="px-4 md:px-20 lg:px-23 space-y-4">
-        {tools.map((tool)=>(
-          <Card key={tool.href}
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
             className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
           >
             <div className="flex items-center gap-x-4">
-              <div className={cn("p-2 w-fit rounded-md", tool.color)}>
-                <tool.icon className={cn("w-8 h-8", tool.bgColor)} /> 
+              <div
+                className={cn(
+                  'p-2 w-fit rounded-md',
+                  tool.color
+                )}
+              >
+                <tool.icon
+                  className={cn('w-8 h-8', tool.bgColor)}
+                />
+              </div>
+              <div className="font-semibold">
+                {tool.label}
               </div>
             </div>
-        </Card>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
         ))}
       </div>
     </div>
